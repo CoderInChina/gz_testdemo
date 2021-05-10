0d6563cc0e1de8d73f8fa5639d39fe76fa7cf58e
xiaoming <xiaoming@qq.com>
2021-05-10 13:19:55 +0800

    // 张三在task_branch 添加了一个分支

--- 01-git测试/src/com/gec/aa/TestDemo1.java
+++ 01-git测试/src/com/gec/aa/TestDemo1.java
@@ -2,22 +2,10 @@ package com.gec.aa;
 
 public interface TestDemo1 {
 
+
 	
-	void addUser();
-	
-	// 张三新加了一个aa方法
-	void aa();
-	
-	// 张三加的aaa_xx 方法
-	void aaa_xxx();
-	
-	// 张三的bb
-	void zhangsan_bb();
-	
-	
-	void lisi_bbb();
-	
-	void test_aaa();
+	// 张三在task_branch 添加了一个分支
+	void test_branch();
 	
 	
 	
